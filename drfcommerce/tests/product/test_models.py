368b67a113fa10e6f5f9bd6a01ea7c58c3b0c0d0
AlexisDevGrp <alexisdevgrp@gmail.com>
2023-03-06 06:15:22 +0100

    version 1.02

--- drfcommerce/tests/product/test_models.py
+++ drfcommerce/tests/product/test_models.py
@@ -1,4 +1,5 @@
 import pytest
+from django.core.exceptions import ValidationError
 
 pytestmark = pytest.mark.django_db
 
@@ -7,7 +8,7 @@ class TestCategoryModel:
     def test_str_method(self, category_factory):
         # Arrange
         # Act
-        obj = category_factory(name='test_cat')
+        obj = category_factory(name="test_cat")
         # Assert
         assert obj.__str__() == "test_cat"
 
@@ -16,7 +17,7 @@ class TestBrandModel:
     def test_str_method(self, brand_factory):
         # Arrange
         # Act
-        obj = brand_factory(name='test_brand')
+        obj = brand_factory(name="test_brand")
         # Assert
         assert obj.__str__() == "test_brand"
 
@@ -25,7 +26,18 @@ class TestProductModel:
     def test_str_method(self, product_factory):
         # Arrange
         # Act
-        obj = product_factory(name='test_prod')
+        obj = product_factory(name="test_product")
         # Assert
-        assert obj.__str__() == "test_prod"
+        assert obj.__str__() == "test_product"
 
+
+class TestProductLineModel:
+    def test_str_method(self, product_line_factory):
+        obj = product_line_factory(sku="12345")
+        assert obj.__str__() == "12345"
+
+    def test_duplicate_order_values(self, product_line_factory, product_factory):
+        obj = product_factory()
+        product_line_factory(order=1, product=obj)
+        with pytest.raises(ValidationError):
+            product_line_factory(order=1, product=obj).clean()

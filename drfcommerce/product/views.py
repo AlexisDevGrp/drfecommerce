a3581d470418a0a5d5f6dec5646e7ac81e6099e0
AlexisDevGrp <alexisdevgrp@gmail.com>
2023-03-25 12:41:26 +0100

    new step

--- drfcommerce/product/views.py
+++ drfcommerce/product/views.py
@@ -1,59 +1,63 @@
-from drf_spectacular.utils import extend_schema
-from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
 from django.db import connection
+from django.db.models import Prefetch
+from drf_spectacular.utils import extend_schema
 from pygments import highlight
 from pygments.formatters import TerminalFormatter
 from pygments.lexers import SqlLexer
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from sqlparse import format
+
 from .models import Brand, Category, Product
 from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
-from django.db.models import Prefetch
 
 
-@extend_schema(responses=CategorySerializer)
 class CategoryViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing all categories
+    """
 
     queryset = Category.objects.all()
 
+    @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
         return Response(serializer.data)
 
 
-@extend_schema(responses=BrandSerializer)
 class BrandViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing all brands
+    """
 
     queryset = Brand.objects.all()
 
+    @extend_schema(responses=BrandSerializer)
     def list(self, request):
         serializer = BrandSerializer(self.queryset, many=True)
         return Response(serializer.data)
 
 
-@extend_schema(responses=ProductSerializer)
 class ProductViewSet(viewsets.ViewSet):
-    # Object is the default manager which is replaced by isactive
-    # queryset = Product.objects.all()
-    # queryset = Product.isactive.all()
-    # should do the same without ovveride
+    """
+    A simple Viewset for viewing all products
+    """
+
     queryset = Product.objects.all().isactive()
+
     lookup_field = "slug"
 
     def retrieve(self, request, slug=None):
         serializer = ProductSerializer(
             Product.objects.filter(slug=slug)
             .select_related("category", "brand")
-            .prefetch_related(Prefetch("product_line__product_image")),
+            .prefetch_related(Prefetch("product_line__product_image"))
+            .prefetch_related(Prefetch("product_line__attribute_value__attribute")),
             many=True,
         )
-        # x = self.queryset.filter(slug=slug)
-        # sqlformatted = format(str(x.query), reindent=True)
-        # print(highlight(sqlformatted, SqlLexer(), TerminalFormatter()))
-        # return Response(serializer.data)
-
         data = Response(serializer.data)
+
         q = list(connection.queries)
         print(len(q))
         for qs in q:
@@ -62,6 +66,7 @@ class ProductViewSet(viewsets.ViewSet):
 
         return data
 
+    @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
         return Response(serializer.data)
